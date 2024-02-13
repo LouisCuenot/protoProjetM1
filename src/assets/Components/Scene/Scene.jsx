56d2bb8a9c43d6792e9a986f16ed968e39ef438e
LouisCuenot <70881127+LouisCuenot@users.noreply.github.com>
2024-02-13 14:27:14 +0100

    bonsoir c le proto

--- src/assets/Components/Scene/Scene.jsx
+++ src/assets/Components/Scene/Scene.jsx
@@ -1,9 +1,49 @@
 import React, { useEffect, useRef, useState } from 'react'
 import gsap from 'gsap'
-import { useScroll } from '@react-three/drei'
+import { useScroll, useTexture } from '@react-three/drei'
 import * as THREE from 'three'
 
+const Tente = ({value, scale, aaa, currentYear}) => {
+
+  const tent = useRef()
+
+  useEffect(()=>{
+
+    if(currentYear === 0){
+
+      gsap.to(tent.current.material.color,{
+        r:1,
+        g:0.5 ,
+        b:0.1,
+        duration:2
+      })
+    }else if(currentYear === 1){
+      gsap.to(tent.current.material.color,{
+        r:0,
+        g:1,
+        b:0.5,
+        duration:2
+      })
+    }else if(currentYear === 2){
+      gsap.to(tent.current.material.color,{
+        r:0.5,
+        g:1,
+        b:0,
+        duration:2
+      })
+    }
+  },[currentYear])
+  return(
+      <mesh ref={tent} position={[Math.cos((Math.PI*2)+value)*scale,0,Math.sin((Math.PI*2)+value)*scale]} rotation={[0,Math.PI*0.5,Math.PI*0.25]}>
+          <boxGeometry args={[1,1,1]}/>
+          <meshStandardMaterial transparent color={0xFF0000} opacity={aaa > value ? 1 : 0}/>
+      </mesh>
+  )
+}
+
 const Scene = ({currentYear}) => {
+
+  const aa = useTexture('/bb.jpg')
 
   const [aaa, setAaa] = useState({value:currentYear})
 
@@ -31,22 +71,22 @@ const Scene = ({currentYear}) => {
       console.log(sol.current)
       gsap.to(sol.current.material.color,{
         r:1,
-        g:0.85,
-        b:0.6,
+        g:0.86 ,
+        b:0.57,
         duration:2
       })
     }else if(currentYear === 1){
       gsap.to(sol.current.material.color,{
         r:0,
-        g:1,
-        b:0.89,
+        g:0.9,
+        b:0.9,
         duration:2
       })
     }else if(currentYear === 2){
       gsap.to(sol.current.material.color,{
-        r:1,
+        r:0.6,
         g:1,
-        b:1,
+        b:0.9,
         duration:2
       })
     }
@@ -56,37 +96,42 @@ const Scene = ({currentYear}) => {
 
   return (
     <>
-        <mesh rotation={[0,Math.PI*0.5,Math.PI*0.25]}>
-            <boxGeometry args={[1,1,1]}/>
-            <meshStandardMaterial transparent color={0xFF0000}/>
-        </mesh>
-        <mesh position={[Math.cos((Math.PI*2)+11)*5,0,Math.sin((Math.PI*2)+11)*5]} rotation={[0,Math.PI*0.5,Math.PI*0.25]}>
-            <boxGeometry args={[1,1,1]}/>
-            <meshStandardMaterial transparent color={0xFF0000} opacity={aaa > 11 ? 1 : 0}/>
-        </mesh>
-        <mesh position={[Math.cos((Math.PI*2)+9.8)*5,0,Math.sin((Math.PI*2)+9.8)*5]} rotation={[0,Math.PI*0.5,Math.PI*0.25]}>
-            <boxGeometry args={[1,1,1]}/>
-            <meshStandardMaterial transparent color={0xFF0000} opacity={aaa > 9.8 ? 1 : 0}/>
-        </mesh>
-        <mesh position={[Math.cos((Math.PI*2)+5)*5,0,Math.sin((Math.PI*2)+5)*5]} rotation={[0,Math.PI*0.5,Math.PI*0.25]}>
-            <boxGeometry args={[1,1,1]}/>
-            <meshStandardMaterial transparent color={0xFF0000} opacity={aaa > 5 ? 1 : 0}/>
-        </mesh>
-        <mesh position={[Math.cos((Math.PI*2)+7.5)*5,0,Math.sin((Math.PI*2)+7.5)*5]} rotation={[0,Math.PI*0.5,Math.PI*0.25]}>
-            <boxGeometry args={[1,1,1]}/>
-            <meshStandardMaterial transparent color={0xFF0000} opacity={aaa > 7.5 ? 1 : 0}/>
-        </mesh>
-        <mesh position={[Math.cos((Math.PI*2)+2)*5,0,Math.sin((Math.PI*2)+2)*5]} rotation={[0,Math.PI*0.5,Math.PI*0.25]}>
-            <boxGeometry args={[1,1,1]}/>
-            <meshStandardMaterial transparent color={0xFF0000} opacity={aaa > 2 ? 1 : 0}/>
-        </mesh>
+        <fog attach="fog" color={0xffffff} near={8} far={15} />
+        <Tente currentYear={currentYear} aaa={100} value={0} scale={0}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={11} scale={5}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={9.8} scale={5.5}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={1} scale={7}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={5.3} scale={8}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={1} scale={5}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={12} scale={8}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={7} scale={6}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={3} scale={9}/>
+
+        <Tente  currentYear={currentYear}  aaa={aaa} value={4} scale={7}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={5} scale={5.5}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={6} scale={10}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={7} scale={8}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={8} scale={5}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={9} scale={12}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={10} scale={6}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={11} scale={9}/>
+
+        <Tente  currentYear={currentYear}  aaa={aaa} value={1} scale={7}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={2} scale={5.5}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={3} scale={10}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={4} scale={8}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={8} scale={5}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={9} scale={12}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={10} scale={6}/>
+        <Tente  currentYear={currentYear}  aaa={aaa} value={11} scale={9}/>
+        
         <mesh 
           ref={sol}
           name='sol'
           rotation-x={-Math.PI*0.5}
         >
-          <planeGeometry args={[50,50]}/>
-          <meshBasicMaterial color={0x00FFFF}/>
+          <planeGeometry args={[20,20]}/>
+          <meshBasicMaterial color={0x00FFFF} map={aa}/>
         </mesh>
     </>
   )
