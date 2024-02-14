c37055d14c0e0908007361d0a39a16c2dd140687
Louis Cuenot <70881127+LouisCuenot@users.noreply.github.com>
2024-02-14 17:10:57 +0100

    hoverTente

--- src/assets/Components/Scene/Scene.jsx
+++ src/assets/Components/Scene/Scene.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react'
 import gsap from 'gsap'
-import { useScroll, useTexture } from '@react-three/drei'
+import { Html, useScroll, useTexture } from '@react-three/drei'
 import * as THREE from 'three'
+import './Scene.scss'
+import Im from './Im.png'
 
 const Tente = ({value, scale, aaa, currentYear}) => {
 
@@ -38,6 +40,67 @@ const Tente = ({value, scale, aaa, currentYear}) => {
           <boxGeometry args={[1,1,1]}/>
           <meshStandardMaterial transparent color={0xFF0000} opacity={aaa > value ? 1 : 0}/>
       </mesh>
+  )
+}
+
+const TenteHoverable = ({value, scale, aaa, currentYear}) => {
+
+  const tent = useRef()
+
+  const [isHovered, setIsHovered] = useState(false)
+  const [isClicked, setIsClicked] = useState(false)
+
+  useEffect(()=>{
+
+    if(currentYear === 0){
+
+      gsap.to(tent.current.material.color,{
+        r:1,
+        g:0.5 ,
+        b:0.1,
+        duration:2
+      })
+    }else if(currentYear === 1){
+      gsap.to(tent.current.material.color,{
+        r:0,
+        g:1,
+        b:0.5,
+        duration:2
+      })
+    }else if(currentYear === 2){
+      gsap.to(tent.current.material.color,{
+        r:0.5,
+        g:1,
+        b:0,
+        duration:2
+      })
+    }
+  },[currentYear])
+  return(
+      <>
+        <mesh onClick={()=>setIsClicked(!isClicked)} onPointerEnter={()=>setIsHovered(true)} onPointerLeave={()=>setIsHovered(false)} ref={tent} position={[Math.cos((Math.PI*2)+value)*scale,0,Math.sin((Math.PI*2)+value)*scale]} rotation={[0,Math.PI*0.5,Math.PI*0.25]}>
+            <boxGeometry args={[1,1,1]}/>
+            <meshStandardMaterial transparent color={0xFF0000} opacity={aaa > value ? 1 : 0}/>
+        </mesh>
+        {
+          isHovered &&
+          <mesh position-x={-1}>
+            <sphereGeometry args={[0.7,32,32]} />
+            <meshBasicMaterial color={0xFF9D2A}  />
+          </mesh>
+        }
+        
+        <Html
+          as='div'
+          position={[-2,1.5,0]}
+          className={`aa ${isClicked ? 't' : ''}`}
+          center
+          prepend
+        >
+          <p>AAAAA</p>
+          <img src={Im} alt="" />
+        </Html>
+      </>
   )
 }
 
@@ -96,7 +159,7 @@ const Scene = ({currentYear, isAnimating}) => {
   return (
     <>
         <fog attach="fog" color={0xffffff} near={8} far={15} />
-        <Tente currentYear={currentYear} aaa={100} value={0} scale={0}/>
+        <TenteHoverable currentYear={currentYear} aaa={100} value={0} scale={0}/>
         <Tente  currentYear={currentYear}  aaa={aaa} value={11} scale={5}/>
         <Tente  currentYear={currentYear}  aaa={aaa} value={9.8} scale={5.5}/>
         <Tente  currentYear={currentYear}  aaa={aaa} value={1} scale={7}/>
